93c97d91f49fca580f426d1a7ce7384480f0c556
Henry <93953570+Henryextreme@users.noreply.github.com>
2024-06-25 21:08:28 -0300

    Add files via upload

--- Henry/PROJETINHO DO MANO DAVAS/JS.js
+++ Henry/PROJETINHO DO MANO DAVAS/JS.js
@@ -1,488 +1,78 @@
-
-const jogos = [
-    {
-        "online": "1",
-        "numero": "0",
-        "nome": "Barony",
-        "foto": "Images/Barony.jpeg",
-        "descricao": "V4.1.0",
-        "link_download": "https://drive.google.com/uc?export=download&id=1DZX7Ix-nl7zifopgnPHxloewJl5BBDKX"
-    },
-    {
-        "online": "0",
-        "numero": "1",
-        "nome": "Six Days in Fallujah",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "2",
-        "nome": "SCP 5K",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "3",
-        "nome": "Demonologist",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "4",
-        "nome": "Baldur's Gate 3",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "5",
-        "nome": "Wolfpack",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "6",
-        "nome": "Interplanetary",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "7",
-        "nome": "ICBM",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "8",
-        "nome": "Wargame:Red Dragon",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "9",
-        "nome": "Europa Universalis IV",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "10",
-        "nome": "Civilization VI",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "11",
-        "nome": "Stormworks:Build and Rescue",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "12",
-        "nome": "Devour",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "13",
-        "nome": "Brick Rigs",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "14",
-        "nome": "RimWorld",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "15",
-        "nome": "Castle Crashers",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "16",
-        "nome": "Moving Out 2",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "17",
-        "nome": "BattleBlock Theater",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "18",
-        "nome": "Worms Revolution",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "19",
-        "nome": "GTFO",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "20",
-        "nome": "The Greatest Penguin Heist of All Time",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "21",
-        "nome": "Party Animals",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "22",
-        "nome": "Sniper Elite 4",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "23",
-        "nome": "Cuphead",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "24",
-        "nome": "Devil May Cry 5",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "25",
-        "nome": "The Front",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "26",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "27",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "28",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "29",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "30",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "31",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "32",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "33",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "34",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "35",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "36",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "37",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "38",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "39",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "40",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "41",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "42",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "43",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "44",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "45",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "46",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "47",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "48",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "49",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "50",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "51",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "52",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "53",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "54",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "55",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "56",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "57",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-    {
-        "online": "0",
-        "numero": "58",
-        "nome": "Phasmophobia",
-        "foto": "link-para-foto-jogo2.jpg",
-        "descricao": "Descrição do Jogo 2",
-        "link": "link-para-jogo2.com"
-    },
-];
-for(x = 0; x < 1000; x++){
-    document.getElementById("jogos").innerHTML += `
-    <div class="jogo">
-        <img class="foto" src="`+ jogos[x].foto + `">
-        <div class="texto">
-            <div class="title">` + jogos[x].nome + `</div>
-            <div class="descricao">` + jogos[x].descricao + `</div>
-        </div>
-        <div class="download">
-            <a href="` + jogos[x].link_download + `" class="download"><i class="gg-software-download"></i></a>
-        </div>
-    </div>`
+let jogos = [];
+online = -1
+function ifonline(x){
+    online = x
+    criarcards()
 }
+async function carregarJogos() {
+    try {
+        const response = await fetch('jogos.json');
+        if (!response.ok) {
+            throw new Error('Erro ao carregar o JSON');
+        }
+        jogos = await response.json();
+        criarcards();
+    } catch (error) {
+        console.error('Erro ao carregar o JSON:', error);
+    }
+}
+
+function criarcard(x) {
+    jogoonline = parseInt(jogos[x].online)
+    if(jogoonline === online || online === -1){
+        document.getElementById("jogos").innerHTML += `
+        <div class="jogo">
+            <img class="foto" src="${jogos[x].foto}">
+            <div class="texto">
+                <div class="title" id="title${x}">${jogos[x].nome}</div>
+                <div class="descricao">${jogos[x].descricao}</div>
+            </div>
+            <div class="download">
+                <a href="${jogos[x].link_download}" class="download"><i class="gg-software-download"></i></a>
+            </div>
+        </div>`;
+        ajustarFontSize(x);
+    }
+}
+
+function criarcards() {
+    document.getElementById("jogos").innerHTML = "";
+    for (let x = 0; x < jogos.length; x++) {
+        criarcard(x);
+    }
+}
+
+function ajustarFontSize(x) {
+    if (jogos[x].nome.length > 5) {
+        document.getElementById("title" + x).style.fontSize = 300 / jogos[x].nome.length + 'px';
+    } else {
+        document.getElementById("title" + x).style.fontSize = '30px';
+    }
+}
+
+function pesquisa() {
+    const barra = document.getElementById("search_bar").value.toLowerCase();
+    document.getElementById("jogos").innerHTML = "";
+    for (let x = 0; x < jogos.length; x++) {
+        const nomeJogo = jogos[x].nome.toLowerCase();
+        if(barra.length == 1 && nomeJogo[0] === barra[0]){
+            criarcard(x);
+        }
+        else {
+            if (nomeJogo.includes(barra) && barra.length != 1) {
+                criarcard(x);
+            }
+        }
+    }
+}
+
+document.getElementById("search_bar").addEventListener('input', () => {
+    const search = document.getElementById("search_bar").value;
+    if (search === '') {
+        criarcards();
+    } else {
+        pesquisa();
+    }
+});
+
+window.onload = carregarJogos;
